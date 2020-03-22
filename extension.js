1be2603a5b8cd5ea17b718f7f9b7146570296e6d
Steven Mey <mail@steven-mey.de>
2020-03-22 03:17:42 +0100

    Changed name for new files

--- extension.js
+++ extension.js
@@ -31,36 +31,56 @@ function enable() {
 
         // kill old xbindkeys-instances
         Util.spawn(["/bin/bash", "-c", "pkill -f 'xbindkeys -f' > /dev/null 2>&1 || true"]);
+        Util.spawn(["/bin/bash", "-c", "pkill 'xbindkeys' > /dev/null 2>&1 || true"]);
 
         var app_config_dir = xbindkeys_dir + "/" + app_name_f;
 
         // Create directory
-        if (! GLib.file_test(app_config_dir, GLib.FileTest.IS_DIR) ) {
-            if (DEBUG) { global.log("xbindkeys: no config set: app_id: '" + app_id + "' app_title: '" + app_title + "' app_name: '" + app_name + "'"); }
-            if (autocreate) {
-                GLib.mkdir_with_parents(app_config_dir, 0750);
-            }
-        }
+        // if (! GLib.file_test(app_config_dir, GLib.FileTest.IS_DIR) ) {
+        //     if (DEBUG) { global.log("xbindkeys: no config set: app_id: '" + app_id + "' app_title: '" + app_title + "' app_name: '" + app_name + "'"); }
+        //     if (autocreate) {
+        //         GLib.mkdir_with_parents(app_config_dir, 0750);
+        //     }
+        // }
 
         // run xbindkeys
         if ( GLib.file_test(app_config_dir + "/" + app_title_f + config_suffix, GLib.FileTest.IS_REGULAR) ) {
-            if (DEBUG) { global.log("xbindkeys: activate: " + app_title); }
+            if (DEBUG) { global.log("xbindkeys: activate via title: " + app_config_dir + "/" + app_title_f + config_suffix); }
             Util.spawn([xbindkeys_bin, '-f', app_config_dir + "/" + app_title_f + config_suffix]);
 
         } else if ( GLib.file_test(app_config_dir + "/" + app_name_f + config_suffix, GLib.FileTest.IS_REGULAR) ) {
-            if (DEBUG) { global.log("xbindkeys: activate: " + app_name); }
+            if (DEBUG) { global.log("xbindkeys: activate via name: " + app_config_dir + "/" + app_name_f + config_suffix); }
             Util.spawn([xbindkeys_bin, '-f', app_config_dir + "/" + app_name_f + config_suffix]);
 
         } else {
+            if (DEBUG) { global.log("xbindkeys: not found: " + app_name_f + " | " + app_title_f); }
+            
+            // xbindkey default
+            Util.spawn([xbindkeys_bin]);
+
             if (autocreate) {
-                GLib.file_set_contents(app_config_dir + "/" + app_name_f + config_suffix, "# AppName: " + app_name);
-                GLib.file_set_contents(app_config_dir + "/" + app_title_f + config_suffix, "# AppTitle: " + app_title);
+                if (! GLib.file_test(app_config_dir, GLib.FileTest.IS_DIR) ) {
+                    if (DEBUG) { global.log("xbindkeys: create directory: " + app_config_dir); }
+                    GLib.mkdir_with_parents(app_config_dir, 0750);
+                }
+
+                var new_file = app_config_dir + "/" + app_title_f + config_suffix + ".DISABLED";
+                if (! GLib.file_test(new_file, GLib.FileTest.IS_REGULAR) ) {
+                    if (DEBUG) { global.log("xbindkeys: create file: " + new_file); }
+                    GLib.file_set_contents(new_file, "# AppTitle: " + app_title);
+                }
+
+                new_file = app_config_dir + "/" + app_name_f + config_suffix + ".DISABLED";
+                if (! GLib.file_test(new_file, GLib.FileTest.IS_REGULAR) ) {
+                    if (DEBUG) { global.log("xbindkeys: create file: " + new_file); }
+                    GLib.file_set_contents(new_file, "# AppName: " + app_name);
+                }
             }
         }
     });
 }
 
 function disable() {
-    global.display.disconnect(_windowCreatedId);
+    global.display.disconnect(_windowFocusId);
     _windowFocusId = null;
 } 
